84d387e0e8c0e29469d4d1e9270c3b41f7811772
Felipe Menegossi <felipe.menegossi@gmail.com>
2018-02-21 19:58:06 +0100

    Evaluation form maybe working

---
+++ src/actions/evaluations/fetch.js
@@ -0,0 +1,67 @@
+import API from '../../api/client'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+} from '../loading'
+
+const api = new API()
+
+export default () => {
+  return (dispatch) => {
+    dispatch({ type: APP_LOADING })
+
+    api.get(`/evaluations`)
+      .then((result) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
+  }
+}
+
+export const fetchOneEvaluation = (evaluationId) =>{
+  return (dispatch) => {
+    dispatch({ type: APP_LOADING })
+
+    api.get(`/evaluations/${evaluationId}`)
+      .then((result) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
+  }
+}
+
+export const fetchStudentEvaluations = (studentId) => {
+  console.log('passoooooooou')
+  return (dispatch) => {
+    dispatch({ type: APP_LOADING })
+
+    api.get(`/evaluations/student/${studentId}`)
+      .then((result) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
+  }
+}
---
+++ src/components/EvaluationForm.js
@@ -0,0 +1,73 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import { connect as subscribeToWebsocket } from '../actions/websocket'
+import createEvaluation from '../actions/evaluations/create'
+
+class EvaluationForm extends PureComponent {
+  componentWillMount() {
+    this.props.subscribeToWebsocket()
+  }
+
+  handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    const { student, currentUser, createEvaluation } = this.props
+    const {date, color, remarks} = event.target
+    const newEvaluation = {
+      studentId: student._id,
+      userId: currentUser._id,
+      date: date.value,
+      color: color.value,
+      remarks: remarks.value
+    }
+    console.log(newEvaluation)
+    createEvaluation(newEvaluation)
+  }
+
+  render() {
+    const { student } = this.props
+
+    return (
+      <div className="Game">
+        <h1>Evaluation Form:</h1>
+
+        <form onSubmit={this.handleFormSubmit}>
+          <label> Evaluation for date:
+            <input type="date" name="date"/>
+          </label>
+          <br/>
+          <label>
+            <input name="color" type="radio" value="red" defaultChecked/> RED
+          </label>
+          <label>
+            <input name="color" type="radio" value="yellow" /> YELLOW
+          </label>
+          <label>
+            <input name="color" type="radio" value="green" /> GREEN
+          </label>
+          <br />
+          <label>
+            Remarks:
+            <input name="remarks" type="text"/>
+          </label>
+
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser }, {}) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps, {
+  subscribeToWebsocket,
+  createEvaluation,
+  push
+})(EvaluationForm)
